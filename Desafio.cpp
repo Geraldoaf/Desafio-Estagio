cf0bd4dadc3a490cb37ed29b3c713d1138a3b6f5
Geraldoaf <geraldoafonso_2010@hotmail.com>
2023-05-25 16:06:15 -0300

    modified:   Desafio.cpp     modified:   dados_titulos.csv

--- Desafio.cpp
+++ Desafio.cpp
@@ -1,50 +1,75 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-class Titulo{
+class Titulo {
 public:
     std::string nome;
     float valor_nominal;
     int prazo;
     float juros;
 
-    Titulo(const std::string _nome,float _valor_nominal,int _prazo, float _juros){
+    Titulo(std::string _nome, float _valor_nominal, int _prazo, float _juros){
         this->nome = _nome;
         this->valor_nominal = _valor_nominal;
         this->prazo = _prazo;
         this->juros = _juros;
-    }
+    };
 };
 
-int main() {
-    std::ifstream inputFile("dados_titulos.csv");
-    if (!inputFile) {
-        std::cerr << "Erro ao abrir o arquivo." << std::endl;
-        return 1;
-    }
-    std::string line;
-    std::vector<std::string> fields;
+std::vector<Titulo> lerTitulosDoCSV(const std::string& nomeArquivo){
+    //
     std::vector<Titulo> titulos;
 
-    while (std::getline(inputFile, line)) {
-        size_t pos = 0;
-        std::string value;
-
-        
-        while ((pos = line.find(',')) != std::string::npos) {
-            value = line.substr(0, pos);
-            fields.push_back(value);
-            line.erase(0, pos + 1);
-        }
-        fields.push_back(line);
-
+    std::ifstream inputFile(nomeArquivo);
+    if (!inputFile) {
+        std::cerr << "Erro ao abrir o arquivo." << "\n";
+        exit(1);
     }
 
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::istringstream iss(line); 
+        std::string field;
 
-    
+        std::getline(iss, field, ','); // Indice/ID
+        std::getline(iss, field, ','); // Nome
+        std::string nome = field;
+
+        std::getline(iss, field, ','); // Valor Nominal
+        float valorNominal = std::stof(field);
+
+        std::getline(iss, field, ','); // Prazo
+        int prazo = std::stoi(field);
+
+        std::getline(iss, field, ','); // Juros
+        float juros = std::stof(field);
+
+        Titulo novo(nome,valorNominal,prazo,juros);
+        titulos.push_back(novo);
+        
+    }
+
     inputFile.close();
+
+    return titulos;
+}
+
+
+
+int main() {
+    std::vector<Titulo> titulos = lerTitulosDoCSV("dados_titulos.csv");
+    int id;
+    std::cin >> id;
+
+    std::cout << "Nome: " << titulos[id - 1].nome << std::endl;
+    std::cout << "Valor Nominal: " << titulos[id - 1].valor_nominal << std::endl;
+    std::cout << "Prazo: " << titulos[id - 1].prazo << std::endl;
+    std::cout << "Juros: " << titulos[id - 1].juros << std::endl;
+    std::cout << std::endl;
     
+
     return 0;
 }
